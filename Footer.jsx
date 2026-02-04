6721d858e5f8d51c3ff52ba9a9e96a17b682a45c
Eekshitha26 <eekshitasai26@gmail.com>
2026-02-04 13:51:44 +0530

    Hello everyone this is msg from the local repository

--- Footer.jsx
+++ Footer.jsx
@@ -7,5 +7,11 @@ const Footer = () => {
 }
 
 
+
 export default Footer<br>
 <p>hello<p>
+
+export default Footer
+Hello i wrote this content from the linux terminal guys can
+now i need to check whether it is updated in the github or not
+(Hello everyone this is msg from the local repository)
